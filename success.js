e22780f6d247224791e4afb37e2d84147faac88f
Jayesh Choudhary <jayeshchoudhary@Jayeshs-MacBook-Pro.local>
2022-07-14 20:40:25 +0530

    fix(repo): add txt file

--- success.js
+++ success.js
@@ -1,2 +1,9 @@
 const args = process.argv.slice(2);
-console.log(args[0]);
+
+const fs = require("fs");
+try {
+  fs.writeFileSync("temp.txt", JSON.stringify(args[0], null, 4));
+  // file written successfully
+} catch (err) {
+  console.error(err);
+}
