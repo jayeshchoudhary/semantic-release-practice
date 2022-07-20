7bd708a9262da7320ee34fc1a305f09e2a9a5a18
jayeshchoudhary <jayeshchoudhary619@gmail.com>
2022-07-21 00:00:46 +0530

    feat(repo)add headerCorrespondence

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const micromatch = require("micromatch");
+const config = require("./config");
 console.log("main project major 3");
+const fs = require("fs");
+const temp = require("conventional-changelog-writer");
 
-console.log(
-  micromatch(
-    [
-      "",
-      "data-manager-ui",
-      "data-manager-ui",
-      "data-manager-ui-remote",
-      "adata-manager-ui",
-      "test",
-      "remote-managed-ui",
-    ],
-    ["!data-manager-ui"]
-  )
-); //=> ['foo', 'bar', 'baz']
+var conventionalCommitsParser = require("conventional-commits-parser");
+const result = conventionalCommitsParser.sync(
+  "fix(title): STUI-31 a title is fixed",
+  config
+);
+
+try {
+  fs.writeFileSync("test.json", JSON.stringify(result, null, 4));
+  // file written successfully
+} catch (err) {
+  console.error(err);
+}
+
+console.log("test");
